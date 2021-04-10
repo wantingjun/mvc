a1d8d84ed7b7740d676efe978ebc30631aa10200
TingjunWan <wtj9631@163.com>
2021-04-10 14:27:37 +0800

    four section

---
+++ process.md
@@ -0,0 +1,2 @@
+1. 控制显示隐藏，不要在js上直接.css()/.show()/.hid2()。而是addClass('xxx')和.removeClass('xxx')要样式和功能分离。
+2. toggleClass
---
+++ src/app2.js
@@ -0,0 +1,17 @@
+import $ from 'jquery'
+
+import './app2.css'
+const $tabBar = $('#app2 .tab-bar')
+const $tabContent = $("#app2 .tab-content")
+$tabBar.on('click','li',(e)=>{
+const $li =$(e.currentTarget)
+$li.addClass("selected").siblings().removeClass("selected")
+const index=$li.index()
+$tabContent.children().eq(index)
+// .css({display:'block'})
+// .siblings().css({display:"none"})
+.addClass('active')
+.siblings().removeClass('active')
+})
+
+$tabBar.children().eq(0).trigger('click')
